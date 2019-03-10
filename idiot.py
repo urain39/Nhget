f9e43906f459d729fadcd0ce4f49e1bb0b390503
urain39 <urain39@qq.com>
2019-03-10 16:06:15 +0800

    update: add idiot.py(batch script) x2.

--- idiot.py
+++ idiot.py
@@ -8,7 +8,6 @@ from nhget import Nhget
 
 _ALIAS_EOF = r''
 _ALIAS_FINISHED = "\xe2\x95"
-_PROMPT_MESSAGE = "idol> "
 _TIME_INTERVAL = 0.5
 _RE_GALLERY_URL = re.compile(r"/g/[0-9]{1,}")
 
@@ -19,20 +18,18 @@ def do_task(queue):
     if not queue.empty():
       gallery_url = queue.get()
 
-      if gallery_url is _ALIAS_FINISHED:
+      if gallery_url == _ALIAS_FINISHED:
         break
 
       N.handle_gallery(gallery_url)
     sleep(_TIME_INTERVAL)
 
-def prompt(message="idiot> "):
-  sys.stdout.write(message)
-  sys.stdout.flush()  # show in time
+def prompt():
   return sys.stdin.readline().strip()
 
 def main():
   taskQueue = Queue()
-  gallery_url = prompt(_PROMPT_MESSAGE)
+  gallery_url = prompt()
 
   Thread(target=do_task, args=(taskQueue,)).start()
 
@@ -40,7 +37,7 @@ def main():
     if _RE_GALLERY_URL.match(gallery_url):
       taskQueue.put(gallery_url)
 
-    gallery_url = prompt(_PROMPT_MESSAGE)
+    gallery_url = prompt()
 
   # Notify the `do_task` we are finished.
   taskQueue.put(_ALIAS_FINISHED)
