c124d25a0a2f37b6de0d46bb0e19634067fc3e70
urain39 <urain39@cyfan.cf>
2019-04-23 05:13:27 +0800

    update: add test.py .

--- test.py
+++ test.py
@@ -1,19 +1,9 @@
 from time import sleep
 from nhget import Nhget
 from random import random
-from retry import retry
 
 N = Nhget()
 
-def retry_when(errors):
-  return retry(errors, 9999, lambda cnt:
-               (
-                 sleep(1000 * random()),
-                 N.__exit__()  # reset
-               )
-         )
-
-@retry_when((Exception,))
 def main():
   N.run("decensored english", 1, 1)
 
